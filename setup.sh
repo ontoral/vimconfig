de17f2e1895b41ea9156bc6af790c0228848922c
Marcus M. Darden <ontoral@gmail.com>
2016-01-21 23:24:37 -0500

    Restore original directory after automated instal.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ln -s ~/.vim/.vimrc ~/.vimrc
-cd ~/.vim
+pushd ~/.vim
 git submodule update --init ~/.vim/bundle/nerdtree
 git submodule update --init ~/.vim/bundle/vim-fugitive
+popd
