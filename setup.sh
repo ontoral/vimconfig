fb4c0ab4da087aec2f6f42b8606a6727a534cd8d
Marcus M. Darden <mmdarden@umich.edu>
2018-02-16 03:09:02 -0500

    Update and link .gvimrc.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 ln -s ~/.vim/.vimrc ~/.vimrc
+ln -s ~/.vim/.gvimrc ~/.gvimrc
 pushd ~/.vim
 git submodule update --init ~/.vim/bundle/nerdtree
 git submodule update --init ~/.vim/bundle/vim-fugitive
